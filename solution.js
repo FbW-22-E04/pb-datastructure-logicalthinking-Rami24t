cf5596d76a8b22b6ecd1271ed0e77bf12597e5db
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-23 13:31:12 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -5,3 +5,8 @@
 // now the array is sorted.
 
 //2.
+// loop the all arrays indexes, 2 items at a time, compare by comparing the first letter (if first letters are equal compare the second and so on)
+// and and move the biggest item to the highest index and the lowest in the lowest;
+// loop again but now with one less index (decrease looping/sorting scope by one)
+// keep doing until looping scope is less than 2 items.
+// now the array is sorted.
