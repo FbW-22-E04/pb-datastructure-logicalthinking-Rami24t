77d3c7a0da902c35205472aff36fc5f233f5e500
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-23 13:06:24 +0200

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,7 @@
+//1. 
+// loop the all arrays indexes, 2 items at a time, compare them and and move the biggest item to the highest index and the lowest in the lowest;
+// loop again but now with one less index (decrease looping/sorting scope by one)
+// keep doing until looping scope is less than 2 items.
+// now the array is sorted.
+
+//2.
